cb9851a031ee25bb94fdc10bb0de7fa0fc667ed3
Ayzeld <130386759+Ayzeld@users.noreply.github.com>
2024-09-17 17:15:05 +0700

    Add files via upload

--- lab22.c
+++ lab22.c
@@ -7,11 +7,11 @@ int main()
 	char ed;
 	printf("Temperature(f,c): ");
 	scanf("%f %c",&tem,&ed);
-	if(ed=='c')
+	if(ed=='c'|| ed =='C')
 	{
 	 printf("%.4f f \n", tem*1.8+32);
 	}
-	else if (ed=='f')
+	else if (ed=='f' || ed=='F')
 	{
 	 printf("%.4f c \n", tem/1.8-32);
 	}
