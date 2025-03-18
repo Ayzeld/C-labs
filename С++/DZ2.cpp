09f282324fd41598778c0cc1bfbef7200ae475bd
Ayzeld <130386759+Ayzeld@users.noreply.github.com>
2025-03-18 16:40:32 +0700

    Update DZ2.cpp

--- С++/DZ2.cpp
+++ С++/DZ2.cpp
@@ -1,14 +1,61 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <cstdlib>
+#include <ctime>
 
-// Функция пузырьковой сортировки
-void bubbleSort(std::vector<char>& arr, int& comparisons, int& swaps) {
-    for (size_t i = 0; i < arr.size() - 1; i++) {
-        for (size_t j = 0; j < arr.size() - i - 1; j++) {
+void FillInc(int A[], int n) {
+    for (int i = 0; i < n; i++) {
+        A[i] = i;
+    }
+}
+
+void FillDec(int A[], int n) {
+    for (int i = 0; i < n; i++) {
+        A[i] = n - i - 1;
+    }
+}
+
+void FillRand(int A[], int n) {
+    for (int i = 0; i < n; i++) {
+        A[i] = rand() % 100;
+    }
+}
+
+int CheckSum(int A[], int n) {
+    int summ = 0;
+    for (int i = 0; i < n; i++) {
+        summ += A[i];
+    }
+    return summ;
+}
+
+int RunNumber(int A[], int n) {
+    if (n <= 1) {
+        return n;
+    }
+    int series = 1;
+    for (int i = 1; i < n; i++) {
+        if (A[i] != A[i - 1]) {
+            series++;
+        }
+    }
+    return series;
+}
+
+void PrintMas(int A[], int n) {
+    for (int i = 0; i < n; i++) {
+        std::cout << A[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void bubbleSort(int A[], int n, int& comparisons, int& swaps) {
+    comparisons = 0; // счетчики 
+    swaps = 0;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
             comparisons++;
-            if (arr[j] > arr[j + 1]) {
-                std::swap(arr[j], arr[j + 1]);
+            if (A[j] > A[j + 1]) {
+                std::swap(A[j], A[j + 1]);
                 swaps++;
             }
         }
@@ -16,44 +63,46 @@ void bubbleSort(std::vector<char>& arr, int& comparisons, int& swaps) {
 }
 
 int main() {
-    // Часть 1: Сортировка массива из ФИО "Янин Алексей"
-    std::vector<char> fio = {'Я', 'н', 'и', 'н', ' ', 'А', 'л', 'е'};
-    int comp = 0, swaps = 0;
+    const int n = 15; // Размер массива
+    int A[n];
+    int M, C; // Переменные для подсчета пересылок и сравнений
+    srand(static_cast<unsigned>(time(0))); // Инициализируем генератор случайных чисел
 
-    std::cout << "Исходный массив: ";
-    for (char c : fio) std::cout << c << " ";
-    std::cout << "\n";
+    // Возрастающий массив
+    std::cout << "Возрастающий массив: ";
+    FillInc(A, n);
+    PrintMas(A, n);
+    M = C = 0; // Сбрасываем счетчики
+    bubbleSort(A, n, M, C);
+    std::cout << "После bubbleSort: ";
+    PrintMas(A, n);
+    std::cout << "Пересылки (M): " << M << ", Сравнения (C): " << C << std::endl;
+    std::cout << "Серия: " << RunNumber(A, n) << std::endl;
+    std::cout << "Контрольная сумма: " << CheckSum(A, n) << std::endl;
 
-    bubbleSort(fio, comp, swaps);
+    // Убывающий массив
+    std::cout << "Убывающий массив: ";
+    FillDec(A, n);
+    PrintMas(A, n);
+    M = C = 0; // Сбрасываем счетчики
+    bubbleSort(A, n, M, C);
+    std::cout << "После bubbleSort: ";
+    PrintMas(A, n);
+    std::cout << "Пересылки (M): " << M << ", Сравнения (C): " << C << std::endl;
+    std::cout << "Серия: " << RunNumber(A, n) << std::endl;
+    std::cout << "Контрольная сумма: " << CheckSum(A, n) << std::endl;
 
-    std::cout << "Отсортированный массив: ";
-    for (char c : fio) std::cout << c << " ";
-    std::cout << "\nСравнения: " << comp << ", Пересылки: " << swaps << "\n\n";
+    // Случайный массив
+    std::cout << "Рандомный массив: ";
+    FillRand(A, n);
+    PrintMas(A, n);
+    M = C = 0; // Сбрасываем счетчики
+    bubbleSort(A, n, M, C);
+    std::cout << "После bubbleSort: ";
+    PrintMas(A, n);
+    std::cout << "Пересылки (M): " << M << ", Сравнения (C): " << C << std::endl;
+    std::cout << "Серия: " << RunNumber(A, n) << std::endl;
+    std::cout << "Контрольная сумма: " << CheckSum(A, n) << std::endl;
 
-    // Часть 2: Таблица трудоемкости для массивов разного размера
-    std::vector<int> sizes = {100, 200, 300, 400, 500};
-    std::cout << "N\tM+C (Убыв.)\tM+C (Случ.)\tM+C (Возр.)\n";
-
-    for (int size : sizes) {
-        std::vector<char> decreasing(size), increasing(size), random(size);
-
-        for (int i = 0; i < size; i++) {
-            decreasing[i] = 'Z' - i; // Убывающий массив
-            increasing[i] = 'A' + i; // Возрастающий массив
-            random[i] = 'A' + rand() % 26; // Случайный массив
-        }
-
-        int comp_dec = 0, swaps_dec = 0;
-        int comp_inc = 0, swaps_inc = 0;
-        int comp_rand = 0, swaps_rand = 0;
-
-        bubbleSort(decreasing, comp_dec, swaps_dec);
-        bubbleSort(increasing, comp_inc, swaps_inc);
-        bubbleSort(random, comp_rand, swaps_rand);
-
-        std::cout << size << "\t" << comp_dec + swaps_dec << "\t\t"
-                  << comp_rand + swaps_rand << "\t\t" << comp_inc + swaps_inc << "\n";
-    }
-
-    return 0;
+    return 0; // Возвращаем 0, чтобы указать на успешное выполнение
 }
